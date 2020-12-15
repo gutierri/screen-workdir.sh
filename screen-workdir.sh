76fddf7bd3d1d6ec2a194ad60f3870a6298c019c
Gutierri Barboza <me@gutierri.me>
2020-12-14 21:40:11 -0300

    remove old path for screen and reading params after -w

--- screen-workdir.sh
+++ screen-workdir.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
-
-if [ -x "$(command -v /usr/bin/screen)" ];
-then
-        SCREEN=/usr/bin/screen
-else
-        SCREEN=/usr/local/bin/screen
-fi
+SCREEN=$(which screen)
 
 function screen(){
         if [[ "$1" = "-w" ]]; then
                 shift
                 if [ -z "$STY" ]; then
-                        $SCREEN -S ${PWD##*/}
+                        $SCREEN -S ${PWD##*/} $@
                 else
                         $SCREEN -AdmS ${PWD##*/} bash
                         echo "Screen session ${PWD##*/} created!"
